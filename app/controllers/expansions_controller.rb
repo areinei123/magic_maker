c5b541633385883d77dd20bcb75e4fdaeed71de3
Fred Chapman <areinei123@gmail.com>
2015-06-15 11:26:37 -0400

    completed create

--- app/controllers/expansions_controller.rb
+++ app/controllers/expansions_controller.rb
@@ -6,4 +6,24 @@ class ExpansionsController < ApplicationController
   def show 
     @expansion = Expansion.find(params[:id])
   end
+
+  def new
+    @expansion = Expansion.new
+  end
+
+  def create
+    @expansion = Expansion.new(expansion_params)
+    if @expansion.save
+      flash[:notice] = "Expansion Created"
+      redirect_to root_path
+    else
+      flash[:notice] = @expansion.errors.full_messages.join("! ")
+      render :new
+    end
+  end
+
+  private
+  def expansion_params
+    params.require(:expansion).permit(:set_name, :set_size, :set_desc)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'expansions#index'
   devise_for :users
-  resources :expansions, only: [:index, :show]
+  resources :expansions, only: [:index, :show, :new, :create]
 end
