384e1e45cbd6ebf8355f39435b091c047bb1f028
Fred Chapman <areinei123@gmail.com>
2015-06-23 12:48:58 -0400

    add first and last name to users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'expansions#index'
-  devise_for :users
+  devise_for :users, :controllers => {registrations: 'registrations'}
   resources :expansions, only: [:index, :show, :new, :create, :edit, :update] do 
     resources :cards, only: [:index, :show, :new, :create, :edit, :update]
   end
