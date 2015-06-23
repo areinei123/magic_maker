0feba11029f04a8ef43f3da6b31ca1c0549d2e8c
Fred Chapman <areinei123@gmail.com>
2015-06-23 11:32:45 -0400

    Added card update functionality

--- spec/features/cards/create_spec.rb
+++ spec/features/cards/create_spec.rb
@@ -3,15 +3,16 @@ require 'rails_helper'
 feature 'expasion create' do
   let!(:user) {FactoryGirl.create(:user)}
   let!(:expansion) {FactoryGirl.create(:expansion)}
-  scenario 'create an expasion' do
+  scenario 'create an card' do
     current_user = user
     visit expansion_path(expansion)
     expect(page).to have_content('Add a Card')
     click_on 'Add a Card'
     fill_in :card_name, with: "Astral Conflux"
-    fill_in :card_mana_cost, with: "1U"
+    fill_in :card_mana_cost, with: "1{U}"
     fill_in :card_main_type, with: "Enchantment"
     fill_in :card_rules_text, with: "Pay 1 life: Destroy target enchantment"
+    
     click_on 'Create'
     expect(page).to have_content('Astral Conflux')
   end
